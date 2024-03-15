a0a1ad3983677ea2383cebc2e14e71cee36cb502
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2024-03-15 12:23:08 +0800

    完成登入資訊輸入內容html架構

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,27 @@
-import './Login.scss'
+import "./Login.scss";
 
-function Login(){
+function Login() {
   return (
-    <div className='login-container'>
-      <div className='login-section'>
-        <div className='login-branding'></div>
-        <div className='login-form'></div>
+    <div className="login-container">
+      <div className="login-section">
+        <div className="login-branding"></div>
+        <div className="login-info">
+          <form className="login-form">
+            <div className="login-logo"></div>
+            <div className="environment-notice"></div>
+            <div className="login-input-group">
+              <div className="login-input">
+                <span className="material-icons-outlined">account_circle</span>
+              </div>
+              <div className="login-input">
+                <span className="material-icons-outlined">account_circle</span>
+              </div>
+            </div>
+            <div>
+              <button className="login-btn">Login</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
