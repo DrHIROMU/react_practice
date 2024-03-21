845dbb3b01c8804637c52e70a26a9c0656237dc8
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2024-03-21 21:25:18 +0800

    login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,20 +1,36 @@
 import "./Login.scss";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import HttpsOutlinedIcon from "@mui/icons-material/HttpsOutlined";
+import WarningAmberIcon from "@mui/icons-material/WarningAmber";
 
 function Login() {
   return (
     <div className="login-container">
       <div className="login-section">
-        <div className="login-branding"></div>
+        <div className="login-branding">
+          <div className="system-title">
+            Supplier Relationship Management Portal
+          </div>
+        </div>
         <div className="login-info">
           <form className="login-form">
             <div className="login-logo"></div>
-            <div className="environment-notice"></div>
+            <div className="environment-notice">
+              <WarningAmberIcon className="input-icon-warning" />
+              <div>UAT</div>
+            </div>
             <div className="login-input-group">
               <div className="login-input">
-                <span className="material-icons-outlined">account_circle</span>
+                <AccountCircleOutlinedIcon className="input-icon-normal" />
+                <div className="input-text">
+                  <input type="text"></input>
+                </div>
               </div>
               <div className="login-input">
-                <span className="material-icons-outlined">account_circle</span>
+                <HttpsOutlinedIcon className="input-icon-normal" />
+                <div className="input-text">
+                  <input type="text"></input>
+                </div>
               </div>
             </div>
             <div>
