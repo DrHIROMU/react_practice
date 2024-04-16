edb84ad8b510333b161f3cdb0dd9ea052d21c885
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2024-04-16 10:49:38 +0800

    login頁轉跳至home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,7 @@
-import './Login.scss'
+import "./Home.scss";
 
-function Home(){
-  return (
-    <div className='home-container'>
-      Home
-    </div>
-  );
+function Home() {
+  return <div className="home-container">Home</div>;
 }
 
 export default Home;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,10 +2,34 @@ import "./Login.scss";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import HttpsOutlinedIcon from "@mui/icons-material/HttpsOutlined";
 import WarningAmberIcon from "@mui/icons-material/WarningAmber";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface LoginFormState {
+  username: string;
+  password: string;
+}
 
 function Login() {
-  function login(){
-    alert('login')
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState<LoginFormState>({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  function login(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(formData.username, formData.password);
+    navigate("/home");
   }
 
   return (
@@ -17,7 +41,7 @@ function Login() {
           </div>
         </div>
         <div className="login-info">
-          <form className="login-form">
+          <form className="login-form" onSubmit={login}>
             <div className="login-logo"></div>
             <div className="environment-notice">
               <WarningAmberIcon className="input-icon-warning" />
@@ -27,18 +51,32 @@ function Login() {
               <div className="login-input">
                 <AccountCircleOutlinedIcon className="input-icon-normal" />
                 <div className="input-text">
-                  <input type="text"></input>
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleChange}
+                  ></input>
                 </div>
               </div>
               <div className="login-input">
                 <HttpsOutlinedIcon className="input-icon-normal" />
                 <div className="input-text">
-                  <input type="text"></input>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                  ></input>
                 </div>
               </div>
             </div>
             <div>
-              <button className="login-btn" onClick={login}>Login</button>
+              <button type="submit" className="login-btn">
+                Login
+              </button>
             </div>
           </form>
         </div>
